a7fa928f7dc20eef0191bc9a02f8d94697971e05
Matheusash2 <127854649+Matheusash2@users.noreply.github.com>
2023-06-28 22:06:13 -0300

    Conclusão da tela de login

--- src/views/Login.tsx
+++ src/views/Login.tsx
@@ -1,7 +1,64 @@
+import { useState } from "react";
+import { PublicInput } from '../components/general/PublicInput';
+import { LoginServices } from "../services/LoginServices";
+
+import logo from '../assets/images/logo.svg';
+import loginIcon from '../assets/images/mail.svg';
+import passwordIcon from '../assets/images/key.svg';
+
+const loginServices = new LoginServices();
 
 export const Login = () => {
+
+    const [login, setLogin] = useState('');
+    const [password, setPassword] = useState('');
+    const [error, setError] = useState('');
+    const [loading, setLoading] = useState(false);
+
+    const doLogin = async() => {
+        try {
+            setError('');
+            if(!login || login.trim().length < 5
+                || !password || password.trim().length < 4) {
+                return setError('Favor preencher os campos correctamente.');
+            }
+            setLoading(true);
+            await loginServices.login({login, password})
+            setLoading(false);
+        } catch (e: any) {
+            console.log('Erro ao efetuar o login:', e);
+            setLoading(false);
+            if(e?.response?.data?.message){
+                return setError(e?.response?.data?.message);
+            }
+            return setError('Erro ao efetuar o login, tente novamente.');
+        }
+    }
     
     return (
-        <h1>Login</h1>
+        <div className="container-public">
+            <img src={logo} alt='Logo devameet' className='logo'/>
+            <form>
+                {error && <p className='error'>{error}</p>}
+                <PublicInput 
+                    icon={loginIcon} alt='Email' name='Email'
+                    type='text' modelValue={login} setValue={setLogin}
+                />
+
+                <PublicInput 
+                    icon={passwordIcon} alt='Senha' name='Senha'
+                    type='password' modelValue={password} setValue={setPassword}
+                />
+
+                <button type='button' onClick={doLogin} disabled={loading}>
+                    {loading ? '...Carregando' : 'Login'}
+                </button>
+
+                <div className='link'>
+                    <p>Não possui uma conta?</p>
+                    <a>Faça seu cadastro agora!</a>
+                </div>
+            </form>
+        </div>
     );
 } 
