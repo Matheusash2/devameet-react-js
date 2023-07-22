0a85fe16a9c4a444dbbb542377f9f80b1f66d5a3
Matheusash2 <127854649+Matheusash2@users.noreply.github.com>
2023-07-22 00:09:55 -0300

    Conclusão de edição da room da meet

--- src/components/meet/MeetEdit.tsx
+++ src/components/meet/MeetEdit.tsx
@@ -1,8 +1,9 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { MeetAddEditHeader } from "./MeetAddEditHeader";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import { MeetObjectPicker } from "./MeetObjectPicker";
 import { MeetObjectsroom } from "./MeetObjectsRoom";
+import { MeetServices } from "../../services/MeetServices";
 import objectsJson from "../../assets/objects/objects.json";
 import wallIcon from "../../assets/images/wall.svg";
 import floorIcon from "../../assets/images/floor.svg";
@@ -13,23 +14,65 @@ import couchIcon from "../../assets/images/couch.svg";
 import decorIcon from "../../assets/images/decor.svg";
 import natureIcon from "../../assets/images/nature.svg";
 
+const meetServices = new MeetServices();
+
 export const MeetEdit = () => {
-    const navigate = useNavigate();
     const [index, setIndex] = useState(0);
+    const [id, setId] = useState('');
     const [name, setName] = useState('');
     const [color, setColor] = useState('');
     const [selected, setSelected] = useState<any>({});
     const [objects, setObjects] = useState<any>([]);
+
+    const navigate = useNavigate();
+
+    const isFormInvalid = (
+        !id || id.trim().length < 5 ||
+        !name || name.trim().length < 5 ||
+        !color || color.trim().length < 4
+    );
+
+    const { meetId } = useParams();
+
+    const getMeet = async () => {
+        if (!meetId) {
+            return navigate("/");
+        }
+
+        const result = await meetServices.getMeetById(meetId);
+        if (!result?.data) {
+            return navigate("/");
+        }
+
+        const { _id, name, color } = result.data;
+        setId(_id);
+        setName(name);
+        setColor(color);
+
+        const objectsResult = await meetServices.getMeetObjectsById(meetId);
+        if (objectsResult?.data) {
+            const newObjects = objectsResult?.data?.map((e: any) => {
+                return { ...e, type: e?.name?.split('_')[0] }
+            });
+
+            setObjects(newObjects);
+        }
+
+    }
+
+    useEffect(() => {
+        getMeet();
+    }, [])
 
     const setObject = (object: any) => {
         const newIndex = index + 1;
         object._id = newIndex;
         setIndex(newIndex);
 
-        if(object.selectMultiple === true){
-            const newArray =[...objects, object];
+        if (object.selectMultiple === true) {
+            const newArray = [...objects, object];
             setObjects(newArray);
-        }else{
+        } else {
             const filtered = objects.filter((o: any) => o.type !== object.type);
             filtered.push(object);
             setObjects(filtered);
@@ -44,14 +87,100 @@ export const MeetEdit = () => {
         setSelected(null);
     }
 
+    const rotateObject = (object: any, to: string) => {
+        if (object?._id && (object.type === "chair" || object.type === "couch")) {
+            const index = objects?.indexOf(object);
+            if (to === "left") {
+                switch (object.orientation) {
+                    case "front":
+                        object.orientation = "right";
+                        break;
+                    case "right":
+                        object.orientation = "back";
+                        break;
+                    case "back":
+                        object.orientation = "left";
+                        break;
+                    case "left":
+                        object.orientation = "front";
+                        break;
+                    default: break;
+                }
+            } else if (to === "right") {
+                switch (object.orientation) {
+                    case "front":
+                        object.orientation = "left";
+                        break;
+                    case "left":
+                        object.orientation = "back";
+                        break;
+                    case "back":
+                        object.orientation = "right";
+                        break;
+                    case "right":
+                        object.orientation = "front";
+                        break;
+                    default: break;
+                }
+            }
+            setSelected(object);
+            objects[index] = object;
+            const newArray = [...objects];
+            setObjects(newArray);
+        }
+    }
+
     const goBack = () => {
         return navigate(-1);
     }
 
-    const isFormInvalid = true;
+    const doUpdate = async () => {
+        try {
+            if (isFormInvalid) {
+                return;
+            }
 
-    const doSave = async () => {
+            const body = {
+                name,
+                color,
+                objects
+            }
+            
+            await meetServices.updateMeet(body, id);
+            return navigate('/');
+        } catch (e: any) {
+            if (e?.response?.data?.message) {
+                console.log('Erro ao editar reunião:', e?.response?.data?.message);
+            } else {
+                console.log('Erro ao editar reunião:', e);
+            }
+        }
+    }
 
+    const moveSelected = (event: any, selected: any) => {
+        if (selected && selected._id && selected.type !== "wall" && selected.type !== "floor") {
+            const index = objects?.indexOf(selected);
+
+            switch (event?.key) {
+                case "ArrowUp":
+                    selected.y = selected.y > 0 ? selected.y - 1 : 0;
+                    break;
+                case "ArrowDown":
+                    selected.y = selected.y < 7 ? selected.y + 1 : 7;
+                    break;
+                case "ArrowLeft":
+                    selected.x = selected.x > 0 ? selected.x - 1 : 0;
+                    break;
+                case "ArrowRight":
+                    selected.x = selected.x < 7 ? selected.x + 1 : 7;
+                    break;
+                default: break;
+            }
+            setSelected(selected);
+            objects[index] = selected;
+            const newArray = [...objects];
+            setObjects(newArray);
+        }
     }
 
     return (
@@ -73,12 +202,12 @@ export const MeetEdit = () => {
                 </div>
                 <div className="form">
                     <span onClick={goBack}>Voltar</span>
-                    <button onClick={doSave}
+                    <button onClick={doUpdate}
                         disabled={isFormInvalid}
                         className={isFormInvalid ? 'disabled' : ''}>Salvar</button>
                 </div>
             </div>
-            <MeetObjectsroom objects={objects} selected={selected} setSelected={setSelected} removeObject={removeObject}/>
+            <MeetObjectsroom objects={objects} selected={selected} setSelected={setSelected} removeObject={removeObject} rotateObject={rotateObject} moveSelected={moveSelected} />
         </div>
     );
 }
--- src/components/meet/MeetObjectsRoom.tsx
+++ src/components/meet/MeetObjectsRoom.tsx
@@ -1,15 +1,31 @@
 import trashIcon from "../../assets/images/trash_object.svg";
 import rotateRightIcon from "../../assets/images/rotate_right.svg";
 import rotateLeftIcon from "../../assets/images/rotate_left.svg";
+import { useEffect } from "react";
 
 type MeetObjectsRoomType = {
     objects?:[],
     selected?: any,
     setSelected?(s: any): void,
     removeObject?(o: any): void,
+    rotateObject?(o: any, to: string): void,
+    moveSelected?(event: any, selected: any): void,
 }
 
-export const MeetObjectsroom: React.FC<MeetObjectsRoomType> = ({objects, selected, setSelected, removeObject}) => {
+export const MeetObjectsroom: React.FC<MeetObjectsRoomType> = ({objects, selected, setSelected, removeObject, rotateObject, moveSelected}) => {
+
+    useEffect(() => {
+        const doMove = (event: any) => {
+            moveSelected!!(event, selected);
+        }
+        
+        document.removeEventListener("keyup", doMove);
+        document.addEventListener("keyup", doMove);
+        
+        return () => {
+            document.removeEventListener("keyup", doMove);
+        }
+    }, [selected]);
 
     const getImageFromObject = (object: any) => {
         if (object && object._id) {
@@ -45,36 +61,40 @@ export const MeetObjectsroom: React.FC<MeetObjectsRoomType> = ({objects, selecte
             
         } else if (object.type === 'nature') {
             style += "nature-object ";
-            
+
         }
 
         switch(object.y) {
             case 0: {
-                style += "row-one"
+                style += "row-one "
                 break;
             }
             case 1: {
-                style += "row-two"
+                style += "row-two "
                 break;
             }
             case 2: {
-                style += "row-three"
+                style += "row-three "
                 break;
             }
             case 3: {
-                style += "row-four"
+                style += "row-four "
                 break;
             }
             case 4: {
-                style += "row-five"
+                style += "row-five "
                 break;
             }
             case 5: {
-                style += "row-six"
+                style += "row-six "
                 break;
             }
             case 6: {
-                style += "row-seven"
+                style += "row-seven "
+                break;
+            }
+            case 7: {
+                style += "row-eight "
                 break;
             }
             default:
@@ -82,31 +102,35 @@ export const MeetObjectsroom: React.FC<MeetObjectsRoomType> = ({objects, selecte
         }
         switch(object.x) {
             case 0: {
-                style += "column-one"
+                style += "column-one "
                 break;
             }
             case 1: {
-                style += "column-two"
+                style += "column-two "
                 break;
             }
             case 2: {
-                style += "column-three"
+                style += "column-three "
                 break;
             }
             case 3: {
-                style += "column-four"
+                style += "column-four "
                 break;
             }
             case 4: {
-                style += "column-five"
+                style += "column-five "
                 break;
             }
             case 5: {
-                style += "column-six"
+                style += "column-six "
                 break;
             }
             case 6: {
-                style += "column-seven"
+                style += "column-seven "
+                break;
+            }
+            case 7: {
+                style += "column-eight "
                 break;
             }
             default:
@@ -115,6 +139,9 @@ export const MeetObjectsroom: React.FC<MeetObjectsRoomType> = ({objects, selecte
 
         if (object.name === selected?.name) {
             style += ' selected'
+
+        } if (object.type === 'couch' && (object.orientation === "right" || object.orientation === "left")) {
+            style += ' rotated';
         }
 
         return style; 
@@ -143,17 +170,18 @@ export const MeetObjectsroom: React.FC<MeetObjectsRoomType> = ({objects, selecte
                         onClick={() => selected?.name === object.name ? setSelected!!(null) : setSelected!!(object)} 
                         src={getImageFromObject(object)}
                         className={getClassFromObject(object)}
+                        style={{zIndex: object.zindex}}
                     />)}
             </div>
             <div className="actions">
                 <div className={selected?._id ? "active" : ""}>
                     <img src={trashIcon} onClick={() => selected?._id ? removeObject!!(selected) : null} alt="Excluir" />
                 </div>
-                <div>
-                    <img src={rotateRightIcon} alt="Girar para direita" />
+                <div className={selected?._id && (selected?.type === "chair" || selected?.type === "couch") ? "active" : ""}>
+                    <img src={rotateRightIcon} onClick={() => selected?._id ? rotateObject!!(selected, "right") : null} alt="Girar para direita" />
                 </div>
-                <div>
-                    <img src={rotateLeftIcon} alt="Girar para esquerda" />
+                <div className={selected?._id && (selected?.type === "chair" || selected?.type === "couch") ? "active" : ""}>
+                    <img src={rotateLeftIcon} onClick={() => selected?._id ? rotateObject!!(selected, "left") : null} alt="Girar para esquerda" />
                 </div>
             </div>
         </div>
